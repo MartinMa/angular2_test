bfa0b1150a9ba627348cd0094029415ee408404e
Martin Mädler <martin.maedler@gmail.com>
2016-03-04 15:07:42 +0100

    Task fiddling ...

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,19 +6,33 @@ module.exports = function(grunt) {
       lite: {
         command: 'npm run lite'
       },
+      tsc: {
+        command: 'npm run tsc'
+      },
       tscWatcher: {
         command: 'npm run tsc:w'
       }
     },
     concurrent: {
-      start: ['shell:tscWatcher', 'shell:lite']
+      start: ['shell:tscWatcher', 'shell:lite'],
+      options: {
+				logConcurrentOutput: true
+			}
     }
+    /*ts: {
+      default : {
+        tsconfig: true
+      }
+    }*/
   });
 
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-concurrent');
+  //grunt.loadNpmTasks("grunt-ts");
 
   // Starts the tsc compiler in watch mode
   // and fires up the lite-server.
   grunt.registerTask('start', ['concurrent:start']);
+  
+  grunt.registerTask('tsc', ['shell:tsc']);
 };
