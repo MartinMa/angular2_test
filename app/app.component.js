2ed2d14df6cdc46c67c5f7eb8fcae212db7307c0
Martin Mädler <martin.maedler@gmail.com>
2016-03-17 11:14:08 +0100

    Changed quotes

--- app/app.component.js
+++ app/app.component.js
@@ -36,16 +36,16 @@ System.register(['angular2/core'], function(exports_1, context_1) {
             }());
             exports_1("AppComponent", AppComponent);
             HEROES = [
-                { "id": 11, "name": "Mr. Nice" },
-                { "id": 12, "name": "Narco" },
-                { "id": 13, "name": "Bombasto" },
-                { "id": 14, "name": "Celeritas" },
-                { "id": 15, "name": "Magneta" },
-                { "id": 16, "name": "RubberMan" },
-                { "id": 17, "name": "Dynama" },
-                { "id": 18, "name": "Dr IQ" },
-                { "id": 19, "name": "Magma" },
-                { "id": 20, "name": "Tornado" }
+                { 'id': 11, 'name': 'Mr. Nice' },
+                { 'id': 12, 'name': 'Narco' },
+                { 'id': 13, 'name': 'Bombasto' },
+                { 'id': 14, 'name': 'Celeritas' },
+                { 'id': 15, 'name': 'Magneta' },
+                { 'id': 16, 'name': 'RubberMan' },
+                { 'id': 17, 'name': 'Dynama' },
+                { 'id': 18, 'name': 'Dr IQ' },
+                { 'id': 19, 'name': 'Magma' },
+                { 'id': 20, 'name': 'Tornado' }
             ];
         }
     }
