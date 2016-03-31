6749eb5c3a15d4e7585f8aad2e115f6bd2e542b2
Martin Mädler <martin.maedler@gmail.com>
2016-03-31 16:05:04 +0200

    Add basic routes and navigation

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,16 +3,31 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router'
 
 import {HeroService} from './hero.service';
 import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <a [routerLink]="['Heroes']">Heroes</a>
+    <router-outlet></router-outlet>
   `,
-  directives: [HeroesComponent],
-  providers: [HeroService]
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS, HeroService]
 })
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
 export class AppComponent {
   title = 'Tour of Heroes';
 }
