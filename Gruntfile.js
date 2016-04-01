4a810371cf6c8e3a2988a9e962428ca722046ae1
Martin Mädler <martin.maedler@gmail.com>
2016-04-01 12:41:44 +0200

    Added dedicated library folder :sunny:

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,22 @@ module.exports = function(grunt) {
 				logConcurrentOutput: true
 			}
     },
+    copy: {
+      lib: {
+        expand: true,
+        src: [
+          'node_modules/es6-shim/es6-shim.min.js',
+          'node_modules/systemjs/dist/system-polyfills.js',
+          'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
+          'node_modules/angular2/bundles/angular2-polyfills.js',
+          'node_modules/systemjs/dist/system.src.js',
+          'node_modules/rxjs/bundles/Rx.js',
+          'node_modules/angular2/bundles/angular2.dev.js',
+          'node_modules/angular2/bundles/router.dev.js'],
+        dest: 'lib/',
+        flatten: true
+      }
+    },
     ts: {
       options: {
         'compiler': './node_modules/typescript/bin/tsc'
@@ -39,10 +55,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks("grunt-contrib-copy");
 
   // Starts the tsc compiler in watch mode
   // and fires up the lite-server.
   grunt.registerTask('start', ['concurrent:start']);
   
   grunt.registerTask('tsc', ['shell:tsc']);
+  
+  grunt.registerTask('lib', ['copy:lib']);
 };
