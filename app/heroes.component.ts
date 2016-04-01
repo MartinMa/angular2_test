72c612379f1b7cc00d7b75ecbf12840fb5b626df
Martin Mädler <martin.maedler@gmail.com>
2016-04-01 10:06:43 +0200

    Added navigation to heroes component

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from 'angular2/core';
+import {Router} from 'angular2/router';
+
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service';
@@ -14,7 +16,9 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
   
-  constructor(private _heroService: HeroService) { }
+  constructor(
+    private _heroService: HeroService,
+    private _router: Router) { }
   
   getHeroes() {
     this._heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -26,4 +30,9 @@ export class HeroesComponent implements OnInit {
   }
   
   onSelect(hero: Hero) { this.selectedHero = hero; }
+  
+  gotoDetail() {
+    let link = ['HeroDetail', { id: this.selectedHero.id }];
+    this._router.navigate(link);
+  }
 }
--- dist/heroes.component.js
+++ dist/heroes.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './hero-detail.component', './hero.service'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router', './hero-detail.component', './hero.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,12 +10,15 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, hero_detail_component_1, hero_service_1;
+    var core_1, router_1, hero_detail_component_1, hero_service_1;
     var HeroesComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             },
             function (hero_detail_component_1_1) {
                 hero_detail_component_1 = hero_detail_component_1_1;
@@ -25,8 +28,9 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
             }],
         execute: function() {
             HeroesComponent = (function () {
-                function HeroesComponent(_heroService) {
+                function HeroesComponent(_heroService, _router) {
                     this._heroService = _heroService;
+                    this._router = _router;
                     this.title = 'Tour of Heroes';
                 }
                 HeroesComponent.prototype.getHeroes = function () {
@@ -38,6 +42,10 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
                     this.getHeroes();
                 };
                 HeroesComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
+                HeroesComponent.prototype.gotoDetail = function () {
+                    var link = ['HeroDetail', { id: this.selectedHero.id }];
+                    this._router.navigate(link);
+                };
                 HeroesComponent = __decorate([
                     core_1.Component({
                         selector: 'my-heroes',
@@ -45,7 +53,7 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
                         styleUrls: ['app/heroes.component.css'],
                         directives: [hero_detail_component_1.HeroDetailComponent]
                     }), 
-                    __metadata('design:paramtypes', [hero_service_1.HeroService])
+                    __metadata('design:paramtypes', [hero_service_1.HeroService, router_1.Router])
                 ], HeroesComponent);
                 return HeroesComponent;
             }());
